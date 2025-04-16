290208cd34eb06f43c2dbda38bcdfa8e40038397
Sudip <143170898+Sudip9896@users.noreply.github.com>
2025-04-16 12:39:44 +0530

    sample data

--- app/src/main/java/com/raj/assigment_mvvm/view/UserInfo.kt
+++ app/src/main/java/com/raj/assigment_mvvm/view/UserInfo.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +13,7 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.raj.assigment_mvvm.Adapter.CustomView_Adpater
 import com.raj.assigment_mvvm.R
